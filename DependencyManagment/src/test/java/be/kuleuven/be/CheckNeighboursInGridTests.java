df303517420e66fe27150c3b86cc80e76f23e321
ThiboDeBelie <thibo.debelie@student.uhasselt.be>
2024-03-12 19:10:21 +0100

    Gitignore in main folder, overwrite old one

--- DependencyManagment/src/test/java/be/kuleuven/be/CheckNeighboursInGridTests.java
+++ DependencyManagment/src/test/java/be/kuleuven/be/CheckNeighboursInGridTests.java
@@ -1,2 +1,10 @@
-package be.kuleuven.be;public class CheckNeighboursInGridTests {
+package be.kuleuven.be;
+
+import org.junit.jupiter.api.Test;
+
+public class CheckNeighboursInGridTests {
+    @Test
+    public void verwachting(){
+        CheckNeighboursInGrid test = new CheckNeighboursInGrid();
+    }
 }
