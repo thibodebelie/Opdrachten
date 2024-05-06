df28c2db52dd126ce34ef9511db8d0ccc1b9d1e1
ThiboDeBelie <thibo.debelie@student.uhasselt.be>
2024-05-06 18:43:01 +0200

    opdracht 11: concurrency

--- CandycrushJavaFXstartProject/candycrush/src/main/java/be/kuleuven/candycrush/model/Board.java
+++ CandycrushJavaFXstartProject/candycrush/src/main/java/be/kuleuven/candycrush/model/Board.java
@@ -5,16 +5,22 @@ import com.google.common.collect.Multimaps;
 import javafx.geometry.Pos;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Function;
 import com.google.common.collect.Multimap;
 
 public class Board <T>{
 
-    private Map<Position , T> list = new HashMap<>();
+    private volatile Map<Position , T> list = new ConcurrentHashMap<Position, T>() {
+    };
 
-    private Multimap<T, Position> reverseList = ArrayListMultimap.create();
+    private volatile Multimap<T, Position> reverseList = ArrayListMultimap.create();
 
-    BoardSize boardSize;
+    public BoardSize boardSize;
 
     public Board(BoardSize boardSize, Function<Position , T> CellCreator) {
         this.boardSize = boardSize;
@@ -33,14 +39,14 @@ public class Board <T>{
         return list;
     }
 
-    public T getCellAt(Position position) {
+    public synchronized T getCellAt(Position position) {
         if (position.toIndex() <= list.size()) {
             return list.get(position);
         } else {
             throw new IndexOutOfBoundsException();
         }
     }
-    public void replaceCellAt(Position position, T newCell){
+    public synchronized void replaceCellAt(Position position, T newCell){
         if (position.toIndex() <= list.size()) {
             T temp = list.get(position);
             reverseList.remove(temp , position);
@@ -50,7 +56,7 @@ public class Board <T>{
             throw new IndexOutOfBoundsException();
         }
     }
-    public void fill(Function<Position , T> cellCreator){
+    public synchronized void fill(Function<Position , T> cellCreator){
         for(int i = 0 ; i< boardSize.height();i++){
             for(int j = 0; j <boardSize.width();j++){
                 Position position = new Position(i,j , getBoardSize());
@@ -60,7 +66,7 @@ public class Board <T>{
         }
     }
 
-    public void copyTo(Board<T> otherBoard){
+    public synchronized void copyTo(Board<T> otherBoard){
         if(boardSize.equals(otherBoard.boardSize)){
             for(int i = 0; i< boardSize.height(); i++){
                 for(int j = 0 ; j< boardSize.width() ; j++){
@@ -73,7 +79,7 @@ public class Board <T>{
             throw new IllegalArgumentException("Different boardsize");
         }
     }
-    public List<Position> getPositionsOfElement(T cell){
+    public synchronized List<Position> getPositionsOfElement(T cell){
         return Collections.unmodifiableList((List<Position>) reverseList.get(cell));
     }
 
---
+++ CandycrushJavaFXstartProject/candycrush/src/main/java/be/kuleuven/candycrush/model/MultithreadingClient.java
@@ -0,0 +1,49 @@
+package be.kuleuven.candycrush.model;
+
+import be.kuleuven.candycrush.model.candies.*;
+
+import java.util.Random;
+
+public class MultithreadingClient extends Thread {
+    private Board<Candy> board= new Board<>(new BoardSize(5,5), this::getRandomCandy);
+    private Thread thread1;
+    private Thread thread2;
+
+    public void run(){
+        Random random = new Random();
+        while (true){
+            int x = random.nextInt(board.getBoardSize().width());
+            int y = random.nextInt(board.getBoardSize().height());
+            var candy = getRandomCandy(null);
+            board.replaceCellAt(new Position(x, y, board.getBoardSize()) , candy);
+            System.out.println("The following thread: " + this.getName()+ " changed position: " + x + "," + y + ", To Candy: " + candy);
+            try {
+                Thread.sleep(random.nextLong(1000,5000));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
+    private  Candy getRandomCandy(Position position){
+        Random random = new Random();
+        int rand_int = random.nextInt(9) + 1;
+        Candy randomCandy = switch (rand_int) {
+            case 1 -> new MultiCandy();
+            case 2 -> new RareCandy();
+            case 3 -> new RowSnapper();
+            case 4 -> new TurnMaster();
+            default -> new normalCandy(random.nextInt(4));
+        };
+
+        return randomCandy;
+    }
+
+    public static void main(String[] args){
+        MultithreadingClient thread1 = new MultithreadingClient();
+        MultithreadingClient thread2 = new MultithreadingClient();
+        thread1.start();
+        thread2.start();
+    }
+}
